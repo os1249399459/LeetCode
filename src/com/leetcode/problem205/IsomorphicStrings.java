3c7ea6a4b2bd0ddbe69d13de26d9f64de164dc73
李超 <1249399459@qq.com>
2021-05-19 10:42:43 +0800

    problem 205

--- src/com/leetcode/problem205/IsomorphicStrings.java
+++ src/com/leetcode/problem205/IsomorphicStrings.java
@@ -1,5 +1,29 @@
 package com.leetcode.problem205;
 
-public class IsomorphicStrings {
+import java.util.HashMap;
 
+public class IsomorphicStrings {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        HashMap<Character, Character> hashMap = new HashMap<>();
+        for (int i = 0; i < sArray.length; i++) {
+            if (hashMap.containsKey(sArray[i])) {
+                if (hashMap.get(sArray[i]) != tArray[i]) {
+                    return false;
+                }
+            } else if (hashMap.containsValue(tArray[i])) {
+                return false;
+            } else {
+                hashMap.put(sArray[i], tArray[i]);
+            }
+        }
+        return false;
+    }
 }
